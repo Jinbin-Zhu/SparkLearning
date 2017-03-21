2dc3df993974f9793f70a348d528dfa43c05dc52
Jinbin-Zhu <asjinbinzhu@gmail.com>
2017-03-21 02:08:12 -0700

    WordCount

--- src/spark/learning/test/WordCount.scala
+++ src/spark/learning/test/WordCount.scala
@@ -1,8 +1,31 @@
 package spark.learning.test
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
-  * Created by johann on 3/21/17.
+  * Created by Jinbin Zhu on 3/21/17.
   */
 class WordCount {
 
+
+}
+
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+
+    if(args.length < 1){
+      System.err.println("Usage: <file>")
+      System.exit(1)
+    }
+
+    val conf = new SparkConf().setMaster("local").setAppName("WordCount")
+    val sc = new SparkContext(conf)
+
+    val line = sc.textFile(args(0))
+
+    line.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _).collect.foreach(println)
+
+    sc.stop()
+  }
 }
